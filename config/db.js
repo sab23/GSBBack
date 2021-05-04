be41af77514b4a3506f5cdf15c2da99200e8daaa
sab23 <67158755+sab23@users.noreply.github.com>
2021-05-04 23:51:30 +0200

    bdd

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
-const mysql      = require('mysql');
-
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'gsb'
+const mysql = require("mysql2");
+const connection = mysql.createConnection({ 
+  connectionLimit: 5,
+  host     : 'cj654063-002.dbaas.ovh.net',
+  port     : 35305,
+  user     : 'gsbayache',
+  password : 'gsbayacheSU2020',
+  database : 'gsbayache'
 });
  
 module.exports = connection   // ON EXPORTE CETTE CONNEXION POUR POUVOIR L UTILISER AILLEURS
